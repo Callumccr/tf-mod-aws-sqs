ff9f1c08eafc407f475cfb1a321c75fa3ff438ed
callumccr@gmail.com <jenkins-x@googlegroups.com>
2020-04-26 18:30:31 +0100

    Gitlab README commit

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_sqs_queue" "default" {
   count                       = var.enabled && length(var.queue_name) > 0 ? length(var.queue_name) : 0
-  name                        = "${format("%s%s%s", module.label.id, var.delimiter, element(var.name, count.index))}"
+  name                        = "${format("%s%s%s", module.label.id, var.delimiter, element(var.queue_name, count.index))}"
   visibility_timeout_seconds  = var.visibility_timeout_seconds
   message_retention_seconds   = var.message_retention_seconds
   max_message_size            = var.max_message_size
