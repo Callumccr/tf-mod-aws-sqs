e9a2601d3072976a338214511eb5c36e42303cc5
callumccr@gmail.com <jenkins-x@googlegroups.com>
2020-05-30 21:34:05 +0100

    added mapped output

--- outputs.tf
+++ outputs.tf
@@ -23,3 +23,9 @@ output "queue_arn" {
     0,
   )
 }
+
+
+output "sqs_map" {
+  value       = zipmap(aws_sqs_queue.default.*.id, aws_sqs_queue.default.*.arn)
+  description = "A map of streams sqs ids and their ARNs"
+}
